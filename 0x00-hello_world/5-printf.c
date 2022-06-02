b412065d5271f1c46829dff8a3b8d005d8c202a6
yemkadel <agosifeso@gmail.com>
2022-06-02 20:23:15 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - this functions uses printf
+ *
+ * Description: printing  using printf
+ * Return: the function returns success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
