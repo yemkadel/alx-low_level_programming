2be77cff2408c77be37b06e997bd21acac22ceba
yemkadel <agosifeso@gmail.com>
2022-06-03 19:54:05 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - this is the main fun
+ *
+ * Description: this is the description
+ * Return: this is the return
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	putchar('\n');
+	return (0);
+}
