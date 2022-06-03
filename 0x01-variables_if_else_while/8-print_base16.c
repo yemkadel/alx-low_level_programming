e668c20b697b9705c2ec4acf7c2899715c85bf08
yemkadel <agosifeso@gmail.com>
2022-06-03 23:20:45 +0000

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Description: thisis 
+ * Return: yejed
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
