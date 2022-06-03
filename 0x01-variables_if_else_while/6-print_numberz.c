8412bc60cfc2daaa6cd126aad5b23c449504bf34
yemkadel <agosifeso@gmail.com>
2022-06-03 20:13:14 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main
+ *
+ * Description: the description
+ * Return: the rtu
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+		putchar('\n');
+	return (0);
+}
