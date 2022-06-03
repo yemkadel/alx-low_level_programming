9379ca987ba719c9ccef27382027d4033c995f66
yemkadel <agosifeso@gmail.com>
2022-06-03 22:28:27 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - this is main
+ *
+ * Description: this is the description
+ * Return: the main return 0
+ */
+int main(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x =  i + 1; x < 10; x++)
+		{
+			putchar(i + '0');
+			putchar(x + '0');
+			if ((i == 8) && (x == 9))
+			{
+				putchar('\n');
+			}
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
