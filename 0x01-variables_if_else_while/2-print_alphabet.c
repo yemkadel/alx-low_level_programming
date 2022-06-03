4a1eabf62476a72fbad9027f97b71f6b3195da75
yemkadel <agosifeso@gmail.com>
2022-06-03 18:54:24 +0000

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is main
+ *
+ * Description: this is the description
+ * Return: the main return 0
+ */
+int main(void)
+{
+	for (char x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
