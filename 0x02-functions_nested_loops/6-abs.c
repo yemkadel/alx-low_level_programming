87ffc5213954982783245cc6237743f5133eff0c
yemkadel <agosifeso@gmail.com>
2022-06-07 22:42:00 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - the absolute func
+ * @c: the  int to test
+ *
+ * Description: dec
+ * Return: returns the abs value of c
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+	return (c);
+}
