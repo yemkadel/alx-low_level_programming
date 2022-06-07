ff710382c4d87f05f50b1d18d2adf64f28226f06
vagrant <vagrant@homestead.test>
2022-06-07 21:12:35 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ *
+ * Description: this is already stated
+ * Return: returns success
+ */
+
+void print_alphabet(void)
+{
+	int i, j;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		j =_putchar(i);
+	}
+	j = _putchar('\n');
+
+	return;
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
 
+int main(void)
+{
+	printf("%c", _putchar('x'));
+	return (0);
+}
