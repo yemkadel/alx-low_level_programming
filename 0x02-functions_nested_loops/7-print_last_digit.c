50163db2e18315469fea0b8b931ff88ec7e6d01c
yemkadel <agosifeso@gmail.com>
2022-06-07 22:52:56 +0000

    task7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - this is the func
+ * @c: this the parameter
+ *
+ * Description: descri
+ * Return: the return
+ */
+
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
