7cef9263742ca7ce7242fbc1e1815ac4628d0181
yemkadel <agosifeso@gmail.com>
2022-06-07 22:23:23 +0000

    task 5

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - this is the  function that checks if the char is lowercase
+ *
+ * Return: it returns and interger value
+ */
+
+void _islower(int c)
+{
+	if (c > 'z')
+	{
+		_putchar('0');
+	}
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - this is a sign func
+ *
+ * Return: returns and integer
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}
+	else if (n  == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-1);
+	}
+}
