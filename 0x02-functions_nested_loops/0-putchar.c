266d77b1b3ca30fdd59e0db65a499923c86247bf
vagrant <vagrant@homestead.test>
2022-06-07 20:14:08 +0000

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - this is the main
+ *
+ * Description: this is where the s=desc
+ * Return: return is 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
