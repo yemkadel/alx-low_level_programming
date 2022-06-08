d76dd15f3ddc6f231a03a6250c0b909d5607e40c
yemkadel <agosifeso@gmail.com>
2022-06-08 21:32:49 +0000

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - the main
+ *
+ * Return: return
+ */
+
+int main(void)
+{
+	long i, num, a = 1, b = 2, n = 98;
+
+	printf("%li, %li", a, b);
+	for (i = b; i < n; i++)
+	{
+		num = a + b;
+		a = b;
+		b = num;
+		printf(", %li", num);
+	}
+	printf("\n");
+	return (0);
+}
