cf76585e0af55c35a1c072aebb09c99733d815dd
yemkadel <agosifeso@gmail.com>
2022-06-09 18:07:34 +0000

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isdigit - func
+ * @c: param
+ *
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
