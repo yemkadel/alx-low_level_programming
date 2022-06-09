a6bf346cf864b6f8fa0627eebd31dddff59764bc
yemkadel <agosifeso@gmail.com>
2022-06-09 18:55:15 +0000

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - func
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int  i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
