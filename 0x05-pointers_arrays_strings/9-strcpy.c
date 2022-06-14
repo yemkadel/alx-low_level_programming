0b709882b88d2f0757fca0dce1a680700eae7d87
yemkadel <agosifeso@gmail.com>
2022-06-14 18:54:30 +0000

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copies src to dest
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: char array
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i))
+	{
+		dest[i] = *(src + i);
+		i++;
+	}
+	return dest;
+}
