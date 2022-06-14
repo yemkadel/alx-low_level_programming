b488bd4248fadb50970cd34b417a1d2019ae29a1
yemkadel <agosifeso@gmail.com>
2022-06-14 09:24:20 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints every other char
+ * @str: the string
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int x, i = 0;
+
+	while (*(str + i))
+	{
+		i++;
+	}
+
+	if (i % 2 != 0)
+	{
+		i -= 1;
+	}
+
+	for (x = (i / 2); x < i; x++)
+	{
+		_putchar(*(str + x));
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	char *str;
+
+	str = "01234";
+
+	puts_half(str);
+	return (0);
+}
