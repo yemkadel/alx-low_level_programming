5f866500b63ac328fb0e5fa5983c30378fd9d841
yemkadel <agosifeso@gmail.com>
2022-06-16 17:11:18 +0000

    push

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strncat - conacts two strings
+ * @dest: destination array
+ * @src: source arry
+ * @n: the n value
+ *
+ * Return: char array
+ */
+int _strlen(char *s);
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int dlen = _strlen(dest);
+
+	while (i < n)
+	{
+		dest[dlen] = src[i];
+		i++;
+		dlen++;
+	}
+	return (dest);
+}
+
+/**
+ * _strlen - get string len
+ * @s: char array
+ *
+ * Return: Int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
