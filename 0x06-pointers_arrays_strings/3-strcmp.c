e8f4ad97535ce8e8b2040f4e5a9cf3aa5faeba9d
yemkadel <agosifeso@gmail.com>
2022-06-17 13:11:20 +0000

    push

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,27 +8,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n_s1, n_s2;
-	int i = 0, num;
+	int i = 0, val;
 
-	while (s1[i] && s2[i])
+	while (s1[i] == s2[i])
 	{
-		n_s1 = s1[i];
-		n_s2 = s2[i];
-
-		if (n_s1 > n_s2)
-		{
-			num = 15;
-		}
-		else if (n_s1 < n_s2)
-		{
-			num = -15;
-		}
-		else
-		{
-			num = 0;
-		}
 		i++;
 	}
-	return (num);
+	val = s1[i] - s2[i];
+	return (val);
 }
