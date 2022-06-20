65d3af2f4c52289715e44a976c021d706bae42cb
yemkadel <agosifeso@gmail.com>
2022-06-20 23:12:58 +0000

    push

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - this function fills
+ * @s: src
+ * @b: dest
+ * @n: no f bytes to be filled
+ *
+ * Return: array pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
