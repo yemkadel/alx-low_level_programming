2f9a5e6786e6d1b57c65df2e19bebb0d27246c67
yemkadel <agosifeso@gmail.com>
2022-06-22 21:45:11 +0000

    push

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * checker - checker method
+ * @
+ */
+
+int checker(char *s)
+{
+	
+}
+
+/**
+ * is_palindrome - finds if a string is a palindrome
+ * @s: a pointer to a string
+ *
+ * Return: int
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+}
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,28 @@
 #include "main.h"
 /**
+ * checker - checker
+ * @n: given number
+ * @x: divisor
+ *
+ * Return: int
+ */
+
+int checker(int n, int x)
+{
+	if (x % (n / x) == 0)
+	{
+		if (x * (n / x) == n)
+		{
+			return (x);
+		}
+		else
+		{
+			return (-1);
+		}
+	}
+	return (0 + checker(n, x + 1));
+}
+/**
  * _sqrt_recursion - returns the square root on a number
  * @n: the number
  *
@@ -8,5 +31,12 @@
 
 int _sqrt_recursion(int n)
 {
-	if
+	if (n < 0)
+		return (-1)
+	else if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else
+		return (checker(n, 2));
 }
