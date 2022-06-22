6d877868cc3c509c5e9effa9242755624179887b
yemkadel <agosifeso@gmail.com>
2022-06-22 21:48:53 +0000

    push

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -33,10 +33,10 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
+	else if (n == 0)
 		return (0);
-	if (n == 1)
+	else if (n == 1)
 		return (1);
-
-	return (checker(n, 2));
+	else
+		return (checker(n, 2));
 }
