d81ae82b896e98c9be8257ff180cfc2d89c920dd
yemkadel <agosifeso@gmail.com>
2022-06-25 20:32:27 +0000

    push

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
