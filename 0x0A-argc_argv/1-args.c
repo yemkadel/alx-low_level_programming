66e23d3ab927329fda45beefc34d2b85ab1380a2
yemkadel <agosifeso@gmail.com>
2022-06-25 20:52:49 +0000

    push

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (argv[i] && i <= argc)
+	{
+		i++;
+	}
+	printf("%d\n", i - 1);
+	return (argc);
+}
