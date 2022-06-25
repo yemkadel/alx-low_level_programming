9d1e1c095fbd3d00de1a2d6a6ad66243ec2a83e0
yemkadel <agosifeso@gmail.com>
2022-06-25 22:56:27 +0000

    push

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
