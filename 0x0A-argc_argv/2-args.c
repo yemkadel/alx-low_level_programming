51389d15cc74ce5dab02de30d2463da271b37e79
yemkadel <agosifeso@gmail.com>
2022-06-25 21:03:40 +0000

    push

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - main
+ * @argc: count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int i
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
