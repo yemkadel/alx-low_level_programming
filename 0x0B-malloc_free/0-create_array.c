920cf2d30715967572e5d74015345039c6fb7f67
yemkadel <agosifeso@gmail.com>
2022-06-26 21:23:06 +0000

    push

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,22 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
+	unsigned int i;
 
-	ptr = malloc(sizeof(char) * size);
-	ptr[0] = c;
+	if (size <= 0)
+	{
+		return ('\0');
+	}
+	else
+	{
+		ptr = malloc(sizeof(char) * size);
+		if (!ptr)
+			return ('\0');
+
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+	}
 	return (ptr);
 }
