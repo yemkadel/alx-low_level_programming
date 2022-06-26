01a3876f58d6c74e753dda085b0e2136cad019de
yemkadel <agosifeso@gmail.com>
2022-06-26 00:09:57 +0000

    push

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main
+ * @argc: count
+ * @argv: pointer
+ *
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int cent;
+	int coins = 0;
+
+	if (argc == 1 || argc > 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cent = atoi(argv[1]);
+	while (cent > 0)
+	{
+		if (cent >= 25)
+			cent -= 25;
+		else if (cent >= 10)
+			cent -= 10;
+		else if (cent >= 5)
+			cent -= 5;
+		else if (cent >= 2)
+			cent -= 2;
+		else if (cent >= 1)
+			cent -= 1;
+		coins += 1;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
