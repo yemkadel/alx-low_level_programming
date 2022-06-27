294137c99d9f789097dc71b3bc6b0125c5cfc5d8
yemkadel <agosifeso@gmail.com>
2022-06-27 13:28:04 +0000

    push

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strlen - func gets the lenght of a string
+ * @s: string
+ *
+ * Return: Int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+
+/**
+ * str_concat - string concatenation
+ * @s1: pointer to string 1
+ * @s2: pointer to string 2
+ *
+ * Return: pointer to a new string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *nstr;
+	int j, i, sl = _strlen(s1) + _strlen(s2);
+
+	nstr = (char *) malloc(sizeof(char) * (sl - 1));
+	if (!nstr)
+		return ('\0');
+
+	for (i = 0; i < _strlen(s1); i++)
+		nstr[i] = s1[i];
+
+	for (j = i, i = 0; i < _strlen(s2); j++, i++)
+		nstr[j] = s2[i];
+
+	nstr[j] = '\0';
+	return (nstr);
+}
