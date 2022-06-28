ccee6a77ebee0850368bd0bb5a790fde79d16404
yemkadel <agosifeso@gmail.com>
2022-06-28 17:33:26 +0000

    push

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - func gets the lenght of a string
+ * @s: string
+ *
+ * Return: Int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
+/**
+ * argstostr - arguments to string
+ * @ac: argument count
+ * @av: array pointer
+ *
+ * Return: pointer to char array
+ */
+char *argstostr(int ac, char **av)
+{
+	char *ptr;
+	int i, j, k, sl;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	ptr = (char *) malloc(sizeof(char *) * ac);
+	if (!ptr)
+		return (NULL);
+
+	j = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		sl = _strlen(av[i]);
+		k = 0;
+		while (k < sl)
+		{
+			ptr[j] = av[i][k];
+			j++;
+			k++;
+		}
+		ptr[j++] = '\n';
+	}
+	ptr[j] = '\0';
+
+	return (ptr);
+}
