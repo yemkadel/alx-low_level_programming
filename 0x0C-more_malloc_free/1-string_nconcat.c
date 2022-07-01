2eebe33fa5b50517c6b4ca4476d8d0baad5a4bfc
yemkadel <agosifeso@gmail.com>
2022-07-01 16:38:23 +0000

    push

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * _strlen - len
- * @str: char pointer
- *
- * Return: Int
- */
-int _strlen(char *str)
-{
-	int i = 0;
-
-	if (str[i] != '\0')
-		i++;
-
-	return (i);
-}
 
 /**
  *string_nconcat - concat 2 string
@@ -27,39 +12,37 @@ int _strlen(char *str)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int s1_len, s2_len, i;
+char *str;
+unsigned int s1_len, s2_len, i;
 
 
-	if (s1 == NULL)
-		s1 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	if (s2 == NULL)
-		s2 = "";
+for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+;
 
-	s1_len = _strlen(s1);
-	s2_len = _strlen(s2);
+for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+;
 
-	str = malloc(s1_len + s2_len + 1);
+str = malloc(s1_len + s2_len + 1);
 
 
-	if (n >= s2_len)
-		n = s2_len;
+if (n >= s2_len)
+n = s2_len;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		str[i] = s1[i];
-	}
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
 
-	for (i = 0; i < n; i++)
-	{
-		str[s1_len + i] = s2[i];
-	}
+for (i = 0; i < n; i++)
+str[s1_len + i] = s2[i];
 
-	str[s1_len + n] = '\0';
+str[s1_len + n] = '\0';
 
-	if (str == NULL)
-		return (NULL);
+if (str == NULL)
+return (NULL);
 
-	return (str);
+return (str);
 }
