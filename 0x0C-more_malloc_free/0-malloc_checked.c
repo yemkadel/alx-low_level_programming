b9fca7d308f46312d35b8b00990191aefb1d39f3
yemkadel <agosifeso@gmail.com>
2022-07-01 16:02:32 +0000

    push

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - checked
+ * @b: the size
+ *
+ * Return: address
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
