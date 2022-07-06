7cfeb32b9d25f2683f50fc076ad0182faac66f26
yemkadel <agosifeso@gmail.com>
2022-07-06 23:15:36 +0000

    push

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - array iterator
+ * @array: array
+ * @size: the size of the array
+ * @action: function pointer
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
