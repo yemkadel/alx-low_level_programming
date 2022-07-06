e9661c4c60ed19e17e35121deb48a75aaac50ac1
yemkadel <agosifeso@gmail.com>
2022-07-06 22:57:47 +0000

    push

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - print the fcn
+ * @name: the name
+ * @f: the function pointer
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef _F_POINTER_
+#define _F_POINTER_
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
