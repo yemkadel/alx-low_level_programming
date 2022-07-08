2445c1a496a3de2d12398f7625005ec174b8b229
yemkadel <agosifeso@gmail.com>
2022-07-08 13:33:56 +0000

    push

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - prints the numbers passed
+ * @separator: this is the sperator to use
+ * @n: number of ints passed
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int j;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		j = 0;
+		while (separator[j] != '\0' && separator != NULL && i < (n - 1))
+		{
+			printf("%c", separator[j]);
+			j++;
+		}
+	}
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 #include <stdio.h>
+void print_numbers(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
 #endif
