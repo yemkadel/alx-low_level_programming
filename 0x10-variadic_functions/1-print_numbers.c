8b6536c78330af8d7c7f677e4d0023c12c36dc7d
yemkadel <agosifeso@gmail.com>
2022-07-08 17:14:23 +0000

    push

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
+	va_start(args, n);
 	if (separator)
 	{
-		va_start(args, n);
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(args, int));
