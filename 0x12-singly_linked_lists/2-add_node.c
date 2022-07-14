324ffe4673f420d4ef71783e97422237f0073993
yemkadel <agosifeso@gmail.com>
2022-07-14 22:01:35 +0000

    push

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node to the head
+ * @head: the head to a list
+ * @str: the string to added
+ *
+ * Return: list_t
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node;
+	size_t len;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->str = strdup(str);
+
+	for (len = 0; node->str[len]; len++)
+		;
+	node->len = len;
+	node->next = *head;
+	*head = node;
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,5 +23,6 @@ typedef struct list_s
 /* prototypes */
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /*end _LIST_H_*/
