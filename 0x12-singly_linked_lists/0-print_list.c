33cb290fde44fa8e12c9582a273185870d7c3829
yemkadel <agosifeso@gmail.com>
2022-07-14 21:35:22 +0000

    push

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,24 +2,20 @@
 /**
  * print_list - the func prints elements of a given list
  * @h: list of type list_t
- *
  * Return: size_t
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-	
+	size_t count;
+       
+	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[%d] %s\n", 0, "(nil)");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		count += 1;
 	}
