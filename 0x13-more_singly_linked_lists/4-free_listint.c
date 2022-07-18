fd25b648f8ed52ef52bd56099469f831b190d743
yemkadel <agosifeso@gmail.com>
2022-07-18 21:29:35 +0000

    push

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - free the list from mem
+ * @head: given list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while ((temp == head) != NULL)
+	{
+		head = head->next;
+		free(head->n);
+		free(temp);
+	}
+}
