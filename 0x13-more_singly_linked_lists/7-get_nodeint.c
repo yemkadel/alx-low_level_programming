72bae5308d99ed251142c6bc7f34b9c1caf014e3
yemkadel <agosifeso@gmail.com>
2022-07-19 17:28:07 +0000

    push

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,8 +11,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp;
 
-	if (head == NULL)
-		return (NULL);
+	if (head == 0)
+		return (head);
 	
 	temp = head;
 	while (i < index && temp)
