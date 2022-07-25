cb6e0f53eeaaa4e57ada21e5c229fa2e50872da3
yemkadel <agosifeso@gmail.com>
2022-07-25 20:00:48 +0000

    push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	nread = read(fd, buf, letters);
-	nwrite - write(STDOUT_FILENO, buf, nread);
+	nwrite = write(STDOUT_FILENO, buf, nread);
 
 	close(fd);
 	free(buf);
