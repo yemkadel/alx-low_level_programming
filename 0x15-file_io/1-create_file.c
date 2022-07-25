9270a02f74bdf39b0664846f0b3ca495baa15f84
yemkadel <agosifeso@gmail.com>
2022-07-25 21:03:14 +0000

    push

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,15 +17,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (!text_content)
+		text_content = "";
+
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-	if (text_content != NULL)
-	{
-		nwr = write(fd, text_content, i);
-		if (nwr == -1)
-			return (-1);
-	}
+	nwr = write(fd, text_content, i);
+	if (nwr == -1)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
