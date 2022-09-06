4a2bb15ad9c6a2f403b95732e46a770ee1f4b115
yemkadel <agosifeso@gmail.com>
2022-09-06 00:32:46 +0000

    100 test

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,60 @@
+#include "main.h"
+/**
+ * add - add
+ *
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub
+ *
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul
+ *
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div
+ *
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod
+ *
+ * @a: a
+ * @b: b
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
